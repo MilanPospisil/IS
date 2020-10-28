ff43df3e25333ec000b11df20f1169d6f9aa73fc
Milan Pospisil <arkanus@seznam.cz>
2020-10-28 14:26:31 +0100

    Vraceni zpet

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -12,8 +12,6 @@ function Login(props) {
     let user_input       = React.createRef();
     let password_input   = React.createRef();
 
-    // Toto je test branche Test!!!!!
-
     function login(e) 
     {    
         e.preventDefault();    
